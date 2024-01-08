524485f24f3ccf00bad7b940794d8beaf08357d7
Radhika-222 <wandareradhika@gmail.com>
2024-01-08 22:40:55 +0530

    Assignment2

---
+++ ConsoleAppAss2/ClassLibAss2/UserAuthenticator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibAss2
+{
+    public class UserAuthenticator
+    {
+        private Dictionary<string, string> userDatabase = new Dictionary<string, string>();
+
+        public bool RegisterUser(string username, string password)
+        {
+            if (!userDatabase.ContainsKey(username))
+            {
+                userDatabase.Add(username, password);
+                return true;
+            }
+            return false;
+        }
+
+        public bool LoginUser(string username, string password)
+        {
+            if (userDatabase.TryGetValue(username, out var storedPassword))
+            {
+                return password == storedPassword;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ ConsoleAppAss2/ConsoleAppAss2/UserAuthenticatorTests1.cs
@@ -0,0 +1,40 @@
+﻿using ClassLibAss2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAss2
+{
+    [TestFixture]
+    public class UserAuthenticatorTests1
+    {
+        private UserAuthenticator userAuthenticator;
+
+        [SetUp]
+        public void Setup()
+        {
+            userAuthenticator = new UserAuthenticator();
+        }
+
+        [Test]
+        public void TestUserRegistration()
+        {
+            // Test user registration
+            Assert.IsTrue(userAuthenticator.RegisterUser("newUser", "password123"));
+            Assert.IsFalse(userAuthenticator.RegisterUser("newUser", "password456"));
+        }
+
+        [Test]
+        public void TestUserLogin()
+        {
+            // Test user login
+            userAuthenticator.RegisterUser("existingUser", "password789");
+
+            Assert.IsTrue(userAuthenticator.LoginUser("existingUser", "password789"));
+            Assert.IsFalse(userAuthenticator.LoginUser("nonexistentUser", "invalidPassword"));
+        }
+    }
+}
